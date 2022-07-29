9e64d592f0e2fd5a1b2f23bb4a7c5f904dde84a7
Ilya N. Zykin <ilya.zykin@toptal.com>
2022-07-29 22:53:37 +0300

    fix problem with css image paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
   entry: './src/js/index.js',
@@ -17,24 +18,32 @@ module.exports = {
         test: /\.(png|jpg)$/,
         type: 'asset/resource',
         generator : {
-          publicPath: './',
-          filename : '[name]-[hash][ext][query]'
+          filename: '[name]-[hash][ext]'
         }
       },
       {
         test: /\.css$/,
         use: [
-          'style-loader', 'css-loader'
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './'
+            }
+          },
+          'css-loader'
         ]
       }
     ]
   },
-  plugins: [new HtmlWebpackPlugin(
-    {
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name]-[hash].css'
+    }),
+    new HtmlWebpackPlugin({
       title: 'My App',
       template: 'src/html/index.html',
       filename: 'index.html',
       publicPath: './'
-    }
-  )]
+    })
+  ]
 }
